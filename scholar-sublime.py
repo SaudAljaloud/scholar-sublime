6f371a0093f37fa5a85655beee58ca6031e0cdfb
Saud Aljaloud <sza1g10@ecs.soton.ac.uk>
2017-04-11 00:32:02 +0100

    Update scholar-sublime.py

--- scholar-sublime.py
+++ scholar-sublime.py
@@ -6,7 +6,7 @@ class ScholarsublimeSearchPaperCommand(sublime_plugin.TextCommand):
         for selection in self.view.sel():
             # if the user didn't select anything, search the currently highlighted word
             if selection.empty():
-                text = self.view.word(selection)
+                text = self.view.word(selection.begin())
             text = self.view.substr(selection)
             url = "http://scholar.google.com/scholar?hl=en&q=" + text
             webbrowser.open_new(url)
@@ -19,7 +19,7 @@ class ScholarsublimeSearchAuthorCommand(sublime_plugin.TextCommand):
         for selection in self.view.sel():
             # if the user didn't select anything, search the currently highlighted word
             if selection.empty():
-                text = self.view.word(selection)
+                text = self.view.word(selection.begin())
             text = self.view.substr(selection)
             url = "http://scholar.google.com/citations?hl=en&view_op=search_authors&mauthors=" + text
             webbrowser.open_new(url)
